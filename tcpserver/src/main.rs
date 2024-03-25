fe534d4eeb36aad5d873678cf58c3cd7fcbf4dd2
Donjuanplatinum <donjuan@lecturify.net>
2024-03-25 23:14:48 +0800

    修改:   tcpserver/src/main.rs 增加选项处理

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -51,10 +51,20 @@ fn config() -> String{
     for args in args {
 	vec.push(args);
     }
+    if vec.len() > 1 && vec[1].contains("--help") {
+	help(&vec);
+    }
+
     if vec.len() < 2 {
-	panic!("Usage: {} server_address",&vec[0]);
+	help(&vec);
     }
     let server_address: String = env::args().nth(1).unwrap();
     println!("server_address is {:?}",server_address);
     server_address
+}
+
+fn help(vec: &Vec<String>) ->(){
+    println!("Usage: {} server_address", vec[0]);
+    println!("Example: {} 0.0.0.0:3000", vec[0]);
+    std::process::exit(0);
 }
